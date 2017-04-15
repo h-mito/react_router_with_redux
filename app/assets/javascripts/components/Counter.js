c374dbad1dbf518095f4c82a97e8e8c54974c64b
mito <hiroshi.mito@gmail.com>
2017-04-15 17:27:44 +0900

    Counterコンポーネントを const 風に書き戻す

--- app/assets/javascripts/components/Counter.js
+++ app/assets/javascripts/components/Counter.js
@@ -1,34 +1,24 @@
 import React from "react"
 
-class Counter extends React.Component {
+const Counter = ({counter, onIncrement, onDecrement, history}) => {
 
-  handleClick() {
-    this.props.history.push('/myapp/contact');
-  }
+  return (
+    <div>
+      <button onClick= {(e) => onIncrement()}>
+        Count Up
+      </button>
+      <button onClick= {(e) => onDecrement()}>
+        Count Down
+      </button>
+      <br /><br />
+      <h2>Counter = {counter}</h2>
 
-  render(){
-    let counter = this.props.counter
-    let onIncrement = this.props.onIncrement
-    let onDecrement = this.props.onDecrement
+      <button onClick= {(e) => history.push('/myapp/contact')}>
+        Go To Contact
+      </button>
 
-    return (
-      <div>
-        <button onClick= {(e) => onIncrement()}>
-          Count Up
-        </button>
-        <button onClick= {(e) => onDecrement()}>
-          Count Down
-        </button>
-        <br /><br />
-        <h2>Counter = {counter}</h2>
-
-        <button onClick= {(e) => this.handleClick()}>
-          Go To Contact
-        </button>
-
-      </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Counter
