097612b80e42af252558343155f2272287a7c122
mito <hiroshi.mito@gmail.com>
2017-04-15 17:12:40 +0900

    プログラムからRoute（ページ）を切り替える機能を実装

--- app/assets/javascripts/components/Counter.js
+++ app/assets/javascripts/components/Counter.js
@@ -1,19 +1,34 @@
 import React from "react"
 
-const Counter = ({counter, onIncrement, onDecrement}) =>{
-  return (
-    <div>
-      <button onClick= {(e) => onIncrement()}>
-        Count Up
-      </button>
-      <button onClick= {(e) => onDecrement()}>
-        Count Down
-      </button>
-      <br /><br />
-      <h2>Counter = {counter}</h2>
+class Counter extends React.Component {
 
-    </div>
-  )
+  handleClick() {
+    this.props.history.push('/myapp/contact');
+  }
+
+  render(){
+    let counter = this.props.counter
+    let onIncrement = this.props.onIncrement
+    let onDecrement = this.props.onDecrement
+
+    return (
+      <div>
+        <button onClick= {(e) => onIncrement()}>
+          Count Up
+        </button>
+        <button onClick= {(e) => onDecrement()}>
+          Count Down
+        </button>
+        <br /><br />
+        <h2>Counter = {counter}</h2>
+
+        <button onClick= {(e) => this.handleClick()}>
+          Go To Contact
+        </button>
+
+      </div>
+    )
+  }
 }
 
 export default Counter
